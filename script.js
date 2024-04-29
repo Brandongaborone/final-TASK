cefbdce604ddf7ba05882c0104a09a9ba2e2f2d9
brandongaborone <brandongaborone@gmail.com>
2024-04-29 16:18:30 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+const header = document = getElementById("header");
+header.addEventListener("click",fuction() {header. style . color = "black")
+
+const paragraph = document = getElementById('paragraph')
+
+
+paragraph.innerHTML = " Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations have transformed businesses through digital experiences that bind to their brand.";
+
+const seeAllBtn = document.getElementById('see-all-btn');
+
+seeAllBtn.addEventListener('click',function () => {
+  // Implement your desired action when the "SEE ALL" button is clicked
+  // For example, open a modal window, redirect to a different page, etc.
+  console.log('See All button clicked!');
+});
+
